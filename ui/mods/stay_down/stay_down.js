ab8db2e7f6884ba105cf7ed93b39377ee2a8b9e9
Justin Love <git@JustinLove.name>
2014-03-04 11:29:05 -0600

    proof of concept

---
+++ ui/mods/stay_down/stay_down.js
@@ -0,0 +1,23 @@
+(function() {
+  model.alwaysPlayerListPanel = ko.observable(false).extend({local: 'stay_down_always_players'})
+  model.hoverPlayerListPanel = ko.observable(false)
+  model.showPlayerListPanel = ko.computed(function() {
+    return model.hoverPlayerListPanel() || model.alwaysPlayerListPanel()
+  })
+
+  $('.div_player_list_panel').attr('data-bind', 'event: { ' +
+    'mouseover: function () { model.hoverPlayerListPanel(true) }, ' +
+    'mouseleave: function () { model.hoverPlayerListPanel(false) }, ' +
+    'click: function() {model.alwaysPlayerListPanel(!model.alwaysPlayerListPanel())} }')
+
+  model.alwaysCelestialViewModels = ko.observable(false).extend({local: 'stay_down_always_celestial'})
+  model.hoverCelestialViewModels = ko.observable(false)
+  model.collapseCelestialViewModels = ko.computed(function() {
+    return !(model.hoverCelestialViewModels() || model.alwaysCelestialViewModels())
+  })
+
+  $('.div_planet_list_panel').attr('data-bind', 'event: { ' +
+    'mouseover: function () { model.hoverCelestialViewModels(true) }, ' +
+    'mouseleave: function () { model.hoverCelestialViewModels(false) }, ' + 
+    'click: function() {model.alwaysCelestialViewModels(!model.alwaysCelestialViewModels())} }')
+})()
