c37eba5efb8c3baedb88bf1b3b4bc46aa968b5d0
Justin Love <git@JustinLove.name>
2014-03-05 11:10:35 -0600

    move version display

--- ui/mods/stay_down/stay_down.js
+++ ui/mods/stay_down/stay_down.js
@@ -32,6 +32,8 @@
   $('.div_planet_list_panel .control_chevron_auto')
     .attr('data-bind', 'css: {control_chevron_reverse: alwaysCelestialViewModels}')
 
+  $('#version_info').css('text-align', 'center')
+
   // hotfix for invalid syntax
   $('.div_planet_list_item input[data-bind="click: function () { model.celestialControlModel.($data.index()) }"]').remove()
 })()
