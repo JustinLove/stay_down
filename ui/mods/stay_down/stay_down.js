5412a5734e6aa602661f8a00a3e07d2659e02380
Justin Love <git@JustinLove.name>
2014-03-11 22:06:20 -0500

    version canery

--- ui/mods/stay_down/stay_down.js
+++ ui/mods/stay_down/stay_down.js
@@ -1,4 +1,10 @@
 (function() {
+  if (model.buildVersion() != '62519') {
+    $('body').append('<div class="stay_down_obsolete">Stay Down is Obsolete, please uninstall it.</div>')
+    return
+  }
+
+
   model.alwaysPlayerListPanel = ko.observable(false).extend({local: 'stay_down_always_players'})
   model.hoverPlayerListPanel = ko.observable(false)
   model.showPlayerListPanel = ko.computed(function() {
